67d5150de951dd9d3b1bf1d08f5bdc6c54b54b06
KKRFGROUP <companykkrfpixel@gmail.com>
2025-07-06 13:25:31 +0530

    inital

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "https://heartfelt-cat-57ff20.netlify.app",  
+        "http://localhost:3000",                     
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
